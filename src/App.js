153aee031a5ef981a5e4defa340f51f9c9338eff
moni <solankinitin2912@gmail.com>
2020-01-23 21:04:33 -0800

    asa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Garmi from './img/maxresdefault.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='./maxresdefault.jpg' className="App-logo" alt="logo" />
+        <img src={Garmi} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
