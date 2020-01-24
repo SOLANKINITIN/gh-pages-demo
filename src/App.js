72686ed1ec660c679016af916b8865995b7a98f5
moni <solankinitin2912@gmail.com>
2020-01-23 21:36:37 -0800

    ass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { bounce } from 'react-animations';
 import Garmi from './img/maxresdefault.jpg'
+import God from './img/main god.jpg'
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 
+const styles = {
+  bounce: {
+    animation: 'x 1s infinite',
+    animationName: Radium.keyframes(bounce, 'bounce'),
+    position: 'absolute',
+    left: '32vw',
+    top: '32vh',
+    borderRadius: '100%',
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Garmi} className="App-logo" alt="logo" />
+       <StyleRoot>
+       <img className='nora' src={Garmi}  alt="logo" /> 
+       <img style={styles.bounce} src={God}  alt="logo" /> 
+       </StyleRoot>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
