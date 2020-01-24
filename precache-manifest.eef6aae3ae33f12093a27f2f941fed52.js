e3c4379152a0a04cb8a198291285076d643be2c5
moni <solankinitin2912@gmail.com>
2020-01-23 20:52:01 -0800

    Updates

---
+++ precache-manifest.eef6aae3ae33f12093a27f2f941fed52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa00f3e4c96a0a0a028c73cdd8f6230",
+    "url": "/gh-pages-demo/index.html"
+  },
+  {
+    "revision": "439c5a24eae2c2576d0d",
+    "url": "/gh-pages-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93a0523ef4740f66d0ce",
+    "url": "/gh-pages-demo/static/js/2.253eab6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gh-pages-demo/static/js/2.253eab6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "439c5a24eae2c2576d0d",
+    "url": "/gh-pages-demo/static/js/main.00972dd9.chunk.js"
+  },
+  {
+    "revision": "a61b21c49cbc14ae2315",
+    "url": "/gh-pages-demo/static/js/runtime-main.61e36173.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
